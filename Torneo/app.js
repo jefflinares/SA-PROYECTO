297f15f4c2e30c32849fec6fa64f5d4eb1237c56
Luiskr1993 <luiskr1993@gmial.com>
2020-10-30 02:27:07 -0600

    Microservicio de Torneos

--- Torneo/app.js
+++ Torneo/app.js
@@ -1,4 +1,6 @@
-const PORT = process.env.PORT || 3000;////para utilizar el puerto definido por la nube, sino utilizar el puerto 3000
+
+const config = require('./config');
+const PORT = config.PORT;////para utilizar el puerto definido por la nube, sino utilizar el puerto 3000
 
 var createError = require('http-errors');
 var express = require('express');
@@ -7,9 +9,10 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var usersRouter = require('./routes/usuarios');
 
 var app = express();
+var token = getToken();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -25,7 +28,9 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+//app.use('/users', usersRouter);
+app.use('/usuarios', usersRouter);
+//app.use('/torneos', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -43,4 +48,35 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-module.exports = app;
+
+app.listen(PORT, () => {
+  console.log('Servidor corriendo en puerto '+PORT);
+});
+
+function getToken(){
+  var url = config.JWT_SERVICE_HOST + ":" + config.JWT_SERVICE_PORT + "/token";
+
+    var req = new XMLHttpRequest();
+    req.open("POST",url);
+    req.setRequestHeader('Authorization', 'Bearer ' + access_token);
+    req.setRequestHeader('Content-Type', 'application/json',true);
+    
+    var text = JSON.stringify({"id":config.TORNEOS_SERVICE_ID, "secret":config.TORNEOS_SERVICE_SECRET});
+    var respuesta;
+    
+    req.onreadystatechange = function() {
+        if(req.readyState == 4 && req.status == 201) { 
+            //req.responseText;
+            try{
+                respuesta = JSON.parse(req.responseText);
+                console.log("Token: "+respuesta.jwt);
+                return respuesta.jwt;
+            }catch(err){
+                console.log(err);
+            }
+            return;
+        }  
+    }
+    req.send(text);
+}
+module.exports = {app, token};
