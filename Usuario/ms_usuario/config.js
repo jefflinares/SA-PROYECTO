a609f6995fc7c129b84983e710dc565d7270d60c
jefflinares <diegoforlan102010@hotmail.com>
2020-11-07 11:11:07 -0600

    Prueba usuarios token

--- Usuario/ms_usuario/config.js
+++ Usuario/ms_usuario/config.js
@@ -4,10 +4,10 @@ module.exports = {
 
     NODE_ENV: process.env.NODE_ENV || 'development',
     HOST: process.env.HOST || '127.0.0.1',
-    PORT: process.env.PORT || 3000,
-    DATABASE_HOST: process.env.DATABASE_HOST || 'user_service',
-    DATABASE_NAME: process.env.DATABASE_NAME || '127.0.0.1',
-    DATABASE_PASSWORD: process.env.DATABASE_PASSWORD || 'root',
+    PORT: process.env.PORT || 3001,
+    DATABASE_HOST: process.env.DATABASE_HOST || 'localhost',
+    DATABASE_NAME: process.env.DATABASE_NAME || 'user_service',
+    DATABASE_PASSWORD: process.env.DATABASE_PASSWORD || 'sa1234',
     DATABASE_PORT: process.env.DATABASE_PORT || '3306',
     DATABASE_USER: process.env.DATABASE_USER || 'root',
     
