c67a161d1b5be2fd3ddda926249003d1f4cb5639
Diana Jimenez <dianajimalon@gmail.com>
2020-11-07 06:45:05 -0600

    Actualizacion Docker compose
    se actualizo el docker compose del servicio de usuarios

--- Usuario/ms_usuario/config.js
+++ Usuario/ms_usuario/config.js
@@ -3,10 +3,10 @@ const dotenv = require('dotenv').config();
 module.exports = {
 
     NODE_ENV: process.env.NODE_ENV || 'development',
-    HOST: process.env.HOST || '127.0.0.1',
-    PORT: process.env.PORT || 3000,
-    DATABASE_HOST: process.env.DATABASE_HOST || 'user_service',
-    DATABASE_NAME: process.env.DATABASE_NAME || '127.0.0.1',
+    HOST: process.env.HOST || '0.0.0.0',
+    PORT: process.env.PORT || 3389,
+    DATABASE_HOST: process.env.DATABASE_HOST || '34.123.194.213',
+    DATABASE_NAME: process.env.DATABASE_NAME || 'user_service',
     DATABASE_PASSWORD: process.env.DATABASE_PASSWORD || 'root',
     DATABASE_PORT: process.env.DATABASE_PORT || '3306',
     DATABASE_USER: process.env.DATABASE_USER || 'root',
